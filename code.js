82db8fbdab065d6804c28308c3940009ffb818a6
AlexanderG40 <144170940+AlexanderG40@users.noreply.github.com>
2024-06-02 21:52:51 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+function setPageInfo(){
+    // Remember JavaScript is case sensitive
+    //Add my name to the h1
+    document.getElementById("h1PageTitle").textContent = "Alexander Garcia Bio Info";
+    // Add my major to the major div
+    document.getElementById("divMajor").textContent = "Major: Alexander is studying, Computer Science";
+    // Add my email to the email div
+    document.getElementById("divEmail").textContent = "Email: Alegarc2@uat.edu";
+    // Add my expected grad date to the grad date div
+    document.getElementById("divGradDate").textContent = "Expected Graduation Date: May 2026";
+
+}
